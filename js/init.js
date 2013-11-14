7ed388a35f331e3df5c40e80f8119e093a2bc385
Bob Bennett <rbennett99@southhills.edu>
2013-11-14 12:43:27 -0500

    All 5 plugins functional

---
+++ js/init.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+	var opts = {
+		chars_preset: 'alphanum',
+		align: 'left',
+		width: 11
+    };
+	$('input.display').flapper(opts).val('HELLO THERE').change();
+	
+	var inputValue = $("#flapperInput").text();
+	
+	$('#change').click(function() {
+		$('input.display').val(inputValue).change();
+	});
+	
+	
+	$("#commentForm").validate();
+	
+	$(function(){
+		$("ul#ticker").liScroll();
+	});
+	
+	$(".fancybox").fancybox({
+		openEffect:"elastic",
+		closeEffect:"elastic"		
+	});
+	
+	$(".raptor").raptorize();
+	
+});
